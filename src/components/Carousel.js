d019fd4fc6ee13fbc64b28bb6cf109d45c5b7bc7
Sumit Verma <sumeet0604.sv@gmail.com>
2022-06-02 16:57:20 +0530

    carousel fixed

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+
 // import image from '../assets/image-5.jpg'
-import image1 from '../assets/image-2.jpg'
-import image2 from '../assets/image-7.jpg'
-import image3 from '../assets/image-5.jpg'
+import image1 from '../assets/gallery/img-1.jpg'
+import image2 from '../assets/gallery/img-2.jpg'
+import image3 from '../assets/gallery/img-3.jpg'
+import image4 from '../assets/gallery/img-4.png'
 
 const Carousel = () => {
     let settings = {
@@ -21,37 +21,28 @@ const Carousel = () => {
     return (
         <>
             <section className='px-8 py-12'>
-                <div className='flex flex-col justify-between text-center md:px-40 lg:px-44'>
+                <div className='flex flex-col flex-grow justify-between text-center md:px-40 lg:px-44'>
                     <div className='flex flex-col'
                     >
                         <Slider {...settings}>
                             {/* first */}
-                            <div className='flex flex-wrap px-4 py-4' data-aos="flip-up">
-                                <img src={image1} alt="" />
-                                <div className='bg-[#89926977] flex flex-col'>
-                                    <p className='text-2xl capitalize pt-4 md:py-4 md:text-4xl'>give food to homeless children</p>
-                                    <p className='p-8 md
-                                    text-md'> 21% of India's children are severely underweight. Hungry children can not build the country's future. For India's poor children, the mid-day meal at school is their only proper meal in a day. Feed our children and their hopes by donating mid-day meals to our starving children in need to help them fight starvation and malnutrition, so they don't have to live and learn on a hungry stomach.</p>
-                                </div>
+                            <div className='flex px-4 py-4' data-aos="flip-up">
+                                <img src={image4} alt="" />
                             </div>
                             {/* second */}
-                            <div className='flex flex-wrap px-4' data-aos="flip-up">
+                            <div className='flex px-4' data-aos="flip-up">
                                 <img src={image2} alt="" />
-                                <div className=' bg-[#89926977] flex flex-col'>
-                                    <p className='text-2xl capitalize pt-4 md:py-4 md:text-4xl'> education to homeless children</p>
-                                    <p className='p-8 md
-                                    text-md'>20 Million children in India do not go to school. Millions of children are still denied their right to education because of poverty. Donate for the education of poor children and support them with necessities like tuition fees, school supplies, footwear, transport, etc. Help them build a future that will make them escape poverty passed from generation to generation.</p>
-                                </div>
                             </div>
 
                             {/* third */}
-                            <div className='flex flex-wrap px-4' data-aos="flip-up">
+                            <div className='flex px-4' data-aos="flip-up">
                                 <img src={image3} alt="" />
-                                <div className=' bg-[#89926977] flex flex-col'>
-                                    <p className='text-2xl capitalize pt-4 md:py-4 md:text-4xl'>give smile to homeless children</p>
-                                    <p className='p-8 md
-                                    text-md'> Donate to underfunded programs that are doing great work to provide the poor with necessities like food, shelter, education, senior care, and healthcare. These nonprofits are struggling to continue to help people as they are not receiving the necessary support. Help them with their efforts with your donations.</p>
-                                </div>
+                            </div>
+
+                            {/* fourth */}
+                            {/* third */}
+                            <div className='flex px-4' data-aos="flip-up">
+                                <img src={image1} alt="" />
                             </div>
 
 
